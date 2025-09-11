43b29a8c49543b14c0f27add90bb9d1a11737585
aya <aya@gmail.com>
2025-09-11 10:27:39 -0700

    feat: add UnitOfWork implementation

---
+++ BookingSystem.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using BookingSystem.Core.Models;
+
+namespace BookingSystem.Core.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<User> Users { get; }
+        IRepository<Trip> Trips { get; }
+        IRepository<Reservation> Reservations { get; }
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ BookingSystem.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using BookingSystem.Core.Interfaces;
+using BookingSystem.Core.Models;
+using BookingSystem.Infrastructure.Data;
+
+namespace BookingSystem.Infrastructure.Repositories
+{
+    public class UnitOfWork(AppDbContext context) : IUnitOfWork
+    {
+        public IRepository<User> Users { get; } = new Repository<User>(context);
+        public IRepository<Trip> Trips { get; } = new Repository<Trip>(context);
+        public IRepository<Reservation> Reservations { get; } = new Repository<Reservation>(context);
+
+        public async Task<int> SaveChangesAsync() => await context.SaveChangesAsync();
+
+        public void Dispose() => context.Dispose();
+    }
+
+}
